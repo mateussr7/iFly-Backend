4d71dcced48ce1c8e5b6af941d3a91acfe41ce00
Pedro Henrique <pedro.henrique.riopira@gmail.com>
2021-12-29 00:05:05 -0300

    added Rota entity

---
+++ src/main/java/com/ifly/domain/Rota.java
@@ -0,0 +1,41 @@
+package com.ifly.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "rota")
+@Data
+public class Rota extends GenericEntity{
+    @Id
+    @SequenceGenerator(
+            name = "rota_seq",
+            sequenceName = "rota_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "rota_seq"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_aeroporto_partida")
+    private Aeroporto aeroportoPartida;
+
+    @ManyToOne
+    @JoinColumn(name = "id_aeroporto_chegada")
+    private Aeroporto aeroportoChegada;
+
+    @Column(
+            name = "distancia",
+            columnDefinition = "double precision"
+    )
+    private Float distancia;
+}
